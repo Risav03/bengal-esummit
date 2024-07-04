dc7f08e61f761cff4832e268e87c0854e49cb767
Risav03 <needleweed03@gmail.com>
2024-07-05 01:31:45 +0530

    floating register

--- components/sections/Navbar.tsx
+++ components/sections/Navbar.tsx
@@ -14,10 +14,10 @@ export const Navbar = () => {
   const router = useRouter();
 
   return (
-    <div className='fixed top-0 left-0 z-[50] sm:h-screen max-sm:w-full sm:w-[4rem] flex sm:flex-col items-center justify-center'>
+    <div className='fixed top-0 left-0 z-[999999] sm:h-screen max-sm:w-full sm:w-[4rem] flex sm:flex-col items-center justify-center'>
 
       <button className='absolute top-20 bg-red-500 p-5 w-[6rem] flex justify-end left-0 font-bold hover:bg-red-400 hover:-translate-x-8 -translate-x-10 duration-200 rounded-r-xl max-sm:hidden'><GiArchiveRegister/></button>
-      <button className='absolute top-[90vh] right-5 bg-red-500 p-4 text-2xl font-bold hover:bg-red-400 hover:-translate-y-2 duration-200 sm:hidden rounded-full'><GiArchiveRegister/></button>
+      <button className='absolute top-[85dvh] right-5 bg-red-500 p-4 text-2xl font-bold hover:bg-red-400 hover:-translate-y-2 duration-200 sm:hidden rounded-full'><GiArchiveRegister/></button>
 
       <div className=' sm:w-full sm:rounded-r-xl max-sm:rounded-b-xl max-sm:flex max-sm:flex-row'>
         <button onClick={()=>{router.push("/")}} className='sm:w-20 max-sm:w-[4rem] h-[4rem] max-sm:h-[5rem] sm:border-t-[1px] max-sm:border-l-[1px] flex items-center max-sm:items-end max-sm:pb-4 sm:justify-end max-sm:justify-center sm:pr-4 sm:-translate-x-5 max-sm:-translate-y-5 text-2xl sm:border-b-[1px] max-sm:border-r-[1px] border-slate-800 sm:rounded-tr-xl max-sm:rounded-bl-xl bg-slate-900 hover:bg-slate-800 duration-200 group sm:hover:-translate-x-2 max-sm:hover:-translate-y-2 hover:shadow-red-500/50 shadow-2xl'><IoHomeOutline className='text-red-500 group-hover:text-red-400 duration-200'/></button>
