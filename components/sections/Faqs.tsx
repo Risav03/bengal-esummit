c6d58d18fc848bfcfb1639c6d26adb22413dd8d4
Risav03 <needleweed03@gmail.com>
2024-07-05 22:48:51 +0530

    faq fix

--- components/sections/Faqs.tsx
+++ components/sections/Faqs.tsx
@@ -6,7 +6,7 @@ export const Faqs = () => {
   return (
     <div className='sm:px-20 px-4 sm:py-16 mb-20 w-full flex flex-col items-center justify-center sticky top-0 min-h-screen rounded-t-xl border-t-[2px] border-slate-700 pt-20 bg-gradient-to-b from-slate-900 to-slate-950'>
             <Heading>FAQs</Heading>
-            <div className='sm:w-[50rem] w-[95%] h-[25rem] border-[1px] rounded-xl border-slate-600 shadow-xl shadow-slate-600/50 my-10'>
+            <div className='sm:w-[50rem] w-[95%] border-[1px] rounded-xl border-slate-600 shadow-xl shadow-slate-600/50 my-10'>
             <iframe
           src={pdfFile}
           width="100%"
