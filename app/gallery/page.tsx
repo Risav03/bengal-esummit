1b1008e5bbe7c9f042ac5e80c474ec90e7edcb86
Risav03 <needleweed03@gmail.com>
2024-07-11 01:11:04 +0530

    fix

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -9,9 +9,9 @@ import img1 from "@/assets/gallery/1.jpg"
 import img2 from "@/assets/gallery/2.jpg"
 import img3 from "@/assets/gallery/3.jpg"
 import img4 from "@/assets/gallery/4.jpg"
-import img5 from "@/assets/gallery/5.jpg"
-import img6 from "@/assets/gallery/6.jpg"
-import img7 from "@/assets/gallery/7.jpg"
+// import img5 from "@/assets/gallery/5.jpg"
+// import img6 from "@/assets/gallery/6.jpg"
+// import img7 from "@/assets/gallery/7.jpg"
 // import img8 from "@/assets/gallery/8.nef"
 // import img9 from "@/assets/gallery/9.nef"
 // import img10 from "@/assets/gallery/10.nef"
@@ -34,14 +34,14 @@ export default function Page () {
                 <Image src={img3} alt="img3" className='w-[30rem] max-sm:w-[90%] rounded-xl border-y-[2px] border-blue-600' />
                 <Image src={img4} alt="img4"className='w-[30rem] max-sm:w-[90%] rounded-xl border-y-[2px] border-blue-600'/>
               </div>
-              <div className='flex flex-row gap-10 flex-wrap justify-center'>
+              {/* <div className='flex flex-row gap-10 flex-wrap justify-center'>
                 <Image src={img5} alt="img3" className='w-[30rem] max-sm:w-[90%] rounded-xl border-y-[2px] border-blue-600' />
                 <Image src={img6} alt="img4"className='w-[30rem] max-sm:w-[90%] rounded-xl border-y-[2px] border-blue-600'/>
               </div>
               <div className='flex flex-row gap-10 flex-wrap justify-center'>
                 <Image src={img7} alt="img3" className='w-[30rem] max-sm:w-[90%] rounded-xl border-y-[2px] border-blue-600' />
-                {/* <Image src={img8} alt="img4"className='w-[30rem] max-sm:w-[90%] rounded-xl border-y-[2px] border-blue-600'/> */}
-              </div>
+                <Image src={img8} alt="img4"className='w-[30rem] max-sm:w-[90%] rounded-xl border-y-[2px] border-blue-600'/>
+              </div> */}
               {/* <div className='flex flex-row gap-10 flex-wrap justify-center'>
                 <Image src={img9} alt="img3" className='w-[30rem] max-sm:w-[90%] rounded-xl border-y-[2px] border-blue-600' />
                 <Image src={img10} alt="img4"className='w-[30rem] max-sm:w-[90%] rounded-xl border-y-[2px] border-blue-600'/>
