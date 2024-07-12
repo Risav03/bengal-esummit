3689d81829d5a1c78b6eaf2564423bbf6b2e2da0
Risav03 <needleweed03@gmail.com>
2024-07-12 15:04:28 +0530

    prizes

--- app/prizes/page.tsx
+++ app/prizes/page.tsx
@@ -19,6 +19,64 @@ export default function Page () {
     const secDesc:string = "--to be added later--";
     const thirdDesc:string = "--to be added later--"
 
+    const prizes = [
+        {
+            event: "Investopia",
+            prize: "₹75,000",
+            color: "green"
+        },
+        {
+            event: "Start-up Expo",
+            prize: "₹30,000",
+            color: "yellow"
+        },
+        {
+            event: "Content Creators Conclave",
+            prize: "₹15,000",
+            color: "red"
+        },
+        {
+            event: "Workshops",
+            prize: "Giveaways",
+            color: "blue"
+        },
+        {
+            event: "Keynote Sessions",
+            prize: "Giveaways",
+            color: "fuchsia"
+        },
+        {
+            event: "Mind Masters",
+            prize: "₹15,000",
+            color: "sky"
+        },
+        {
+            event: "Chat JPG",
+            prize: "₹15,000",
+            color: "orange"
+        },
+        {
+            event: "Pitch Perfect",
+            prize: "₹15,000",
+            color: "cyan"
+        },
+        {
+            event: "Social Media Campaign",
+            prize: "Giveaways",
+            color: "teal"
+        },
+        
+        
+    ]
+
+    type PropTypes = {
+        event: string,
+        prize: string,
+        color:string
+    }
+
+    const test:string = "sky"
+
 
   return (
     <main className="bg-black text-white">
@@ -26,32 +84,21 @@ export default function Page () {
             <Navbar/>
         <div className='sm:p-16 px-4 py-20 flex min-h-screen flex-col justify-start items-center relative z-[1]'>
             <Heading>Prizes</Heading>
-            {/* <div className='flex sm:flex-row max-sm:flex-col gap-20 h-[20rem] items-center'>
-
-            {prizeText != "" ? <div className={`p-4 rounded-xl sm:w-[30rem] max-sm:w-[95%] flex flex-col items-center justify-start h-[15rem] border-y-[1px] ${prizeText == firstPrize && "text-yellow-200 border-yellow-400"} ${prizeText == secondPrize && "text-gray-200 border-gray-400"} ${prizeText == thirdPrize && "text-orange-200 border-orange-400"}`}>
-                    <h2 className='text-[1.5rem] font-bold'>{prizeText}</h2>
-                    <p>{prizeDesc}</p>
-                    </div>: <div className='border-y-[1px] text-[2rem] font-bold sm:w-[30rem] max-sm:w-[95%] h-[15rem] flex items-center justify-center border-white p-4 rounded-xl'>
-                        Click on a position
-                        </div>}
-                
-                <div className='grid grid-flow-col grid-cols-3'>
-                    <div onClick={()=>{setPrizeText(secondPrize); setPrizeDesc(secDesc);}} className='cursor-pointer hover:brightness-125 duration-200 hover:-translate-y-2 bg-gradient-to-b from-gray-400/70 text-gray-200/70 to-gray-200/5 relative top-12 rounded-t-xl border-t-[1px] flex items-start justify-center h-[9rem] border-gray-300 sm:w-[7rem] w-[6rem] text-3xl font-semibold pt-8'>
-                        2nd
-                    </div>
-                    <div onClick={()=>{setPrizeText(firstPrize); setPrizeDesc(firstDesc)}} className='cursor-pointer hover:brightness-125 duration-200 hover:-translate-y-2 bg-gradient-to-b from-yellow-500/70 text-yellow-200/70 to-yellow-200/5 rounded-t-xl border-t-[1px] flex items-start justify-center h-[12rem] border-yellow-300 sm:w-[7rem] w-[6rem] text-3xl font-semibold pt-8'>
-                        1st
-                    </div>
-                    <div onClick={()=>{setPrizeText(thirdPrize); setPrizeDesc(thirdDesc)}} className='cursor-pointer hover:brightness-125 duration-200 hover:-translate-y-2 bg-gradient-to-b from-orange-500/70 text-orange-200/70 to-orange-200/5 relative top-20 rounded-t-xl border-t-[1px] flex items-start justify-center h-[7rem] border-orange-300 sm:w-[7rem] w-[6rem] text-3xl font-semibold pt-8'>
-                        3rd
-                    </div>
-                </div>
-
-            </div> */}
-            <div className='w-full items-center flex-col flex justify-center mt-20'>
+            
+            <div className='w-full items-center flex-col flex justify-center my-5'>
                 <h3 className='text-white text-center w-full mx-auto sm:text-[3rem] text-[2rem] font-bold' >Prizes worth </h3>
                 <h3 className='bg-gradient-to-b from-green-800 sm:text-[3.2rem] text-[2.3rem] font-bold via-green-400 to-green-600 text-transparent bg-clip-text'>₹1,50,000</h3>
-                    <h4>--More to be revealed soon!--</h4>
+                
+                <div className='flex flex-wrap gap-10 items-center mt-10'>
+                    {
+                        prizes.map((item:PropTypes)=>(
+                            <div className={`w-[25rem] flex flex-col items-center justify-center h-[12rem] bg-white/30 border-white/50 border-[1px] rounded-xl `}>
+                                <h2 className='font-semibold text-[1.2rem]'>{item.event}</h2>
+                                <h2 className='font-bold text-[1.5rem]'>{item.prize}</h2>
+                                </div>
+                        ))
+                    }
+                </div>
 
             </div>
 
