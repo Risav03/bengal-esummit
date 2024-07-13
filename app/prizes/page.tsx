eab46322b0766b38bd5dce527f1dcf85a2b917c0
Risav03 <needleweed03@gmail.com>
2024-07-13 11:18:26 +0530

    prizes update

--- app/prizes/page.tsx
+++ app/prizes/page.tsx
@@ -5,36 +5,115 @@ import React, { useState } from 'react'
 import { Heading } from '@/components/heading/Heading'
 import { FootComp } from '@/components/sections/footcomp'
 import { Background } from '@/components/UI/Background'
-import pdfFile from "../../assets/documents/investopia.pdf"
 
-export default function Events () {
+export default function Page () {
+
+    const [prizeText, setPrizeText] = useState<string|null>("");
+    const [prizeDesc, setPrizeDesc] = useState<string|null>("");
+
+    const firstPrize:string = "First Prize"
+    const secondPrize:string = "Second Prize"
+    const thirdPrize:string = "Third Prize"
+
+    const firstDesc:string = "--to be added later--";
+    const secDesc:string = "--to be added later--";
+    const thirdDesc:string = "--to be added later--"
+
+    const prizes = [
+        {
+            event: "Investopia",
+            prize: "₹75,000",
+            color: "bg-green-500/60",
+            text: "bg-gradient-to-b from-green-400 via-green-100 to-green-300 text-transparent bg-clip-text"
+        },
+        {
+            event: "Start-up Expo",
+            prize: "₹30,000",
+            color: "bg-yellow-400/60",
+            text: "bg-gradient-to-b from-yellow-400 via-yellow-100 to-yellow-300 text-transparent bg-clip-text"
+        },
+        {
+            event: "Content Creators Conclave",
+            prize: "Giveaways",
+            color: "bg-red-500/60",
+            text: "bg-gradient-to-b from-red-400 via-red-100 to-red-300 text-transparent bg-clip-text"
+        },
+        {
+            event: "Workshops",
+            prize: "Giveaways",
+            color: "bg-blue-400/60",
+            text: "bg-gradient-to-b from-blue-400 via-blue-100 to-blue-300 text-transparent bg-clip-text"
+        },
+        {
+            event: "Keynote Sessions",
+            prize: "Giveaways",
+            color: "bg-fuchsia-400/60",
+            text: "bg-gradient-to-b from-fuchsia-400 via-fuchsia-100 to-fuchsia-300 text-transparent bg-clip-text"
+        },
+        {
+            event: "Mind Masters",
+            prize: "₹15,000",
+            color: "bg-sky-500/60",
+            text: "bg-gradient-to-b from-sky-400 via-sky-100 to-sky-300 text-transparent bg-clip-text"
+        },
+        {
+            event: "Chat JPG",
+            prize: "₹15,000",
+            color: "bg-orange-400/60",
+            text: "bg-gradient-to-b from-orange-400 via-orange-100 to-orange-300 text-transparent bg-clip-text"
+        },
+        {
+            event: "Pitch Perfect",
+            prize: "₹15,000",
+            color: "bg-cyan-400/60",
+            text: "bg-gradient-to-b from-cyan-400 via-cyan-100 to-cyan-300 text-transparent bg-clip-text"
+        },
+        {
+            event: "Social Media Campaign",
+            prize: "₹10,000",
+            color: "bg-teal-400/60",
+            text: "bg-gradient-to-b from-teal-400 via-teal-100 to-teal-300 text-transparent bg-clip-text"
+        },
+        
+        
+    ]
+
+    type PropTypes = {
+        event: string,
+        prize: string,
+        color:string,
+        text:string
+    }
+
+    const test:string = "sky"
+
 
   return (
     <main className="bg-black text-white">
         <Background/>
             <Navbar/>
         <div className='sm:p-16 px-4 py-20 flex min-h-screen flex-col justify-start items-center relative z-[1]'>
-            <div className='sm:px-20 px-4 w-full flex flex-col items-center justify-center sticky sm:-top-16 max-sm:-top-28 min-h-screen rounded-t-xl '>
-                <Heading>Our Events</Heading>
-            </div>
+            <Heading>Prizes</Heading>
             
-            <div className='sm:px-20 px-4 sm:pt-16 pt-20 rounded-t-xl border-t-[2px] min-h-screen border-blue-700 w-full  mt-10 sticky top-0 bg-gradient-to-b from-blue-900 via-blue-700 to-blue-800'>
-            <h1 className='bg-gradient-to-b from-blue-600 via-blue-200 to-blue-400 text-transparent bg-clip-text text-center w-full mx-auto sm:text-[3.3rem] text-[2.3rem] font-bold' >Investopia</h1>
-            <h1 className='bg-gradient-to-b from-blue-100 via-blue-200 to-blue-100 text-transparent bg-clip-text  w-full mx-auto sm:text-[1.3rem] text-[1.5rem] font-bold text-left mb-4 mt-10' >**Template for creation of presentation</h1>
-            <div>
-            <div className='sm:w-full w-[95%] flex items-center justify-center border-[1px] rounded-xl border-slate-600 shadow-xl shadow-slate-600/50 mb-10'>
-            <iframe
-                src={pdfFile}
-                width="100%"
-                height="500px"
-                title="Event Brochure"
-                style={{ border: 'none', borderRadius: '10px' }}
-                ></iframe>
-             </div>
+            <div className='w-full items-center flex-col flex justify-center my-5'>
+                <h3 className='text-white text-center w-full mx-auto sm:text-[3rem] text-[2rem] font-bold' >Prizes worth </h3>
+                <h3 className='bg-gradient-to-b from-green-800 sm:text-[3.2rem] text-[2.3rem] font-bold via-green-400 to-green-600 text-transparent bg-clip-text'>₹1,50,000</h3>
+                
+                <div className='flex flex-wrap gap-10 items-center mt-10 w-full mx-auto justify-center'>
+                    {
+                        prizes.map((item:PropTypes)=>(
+                            <div className={`w-[25rem] flex flex-col hover:brightness-125 duration-200 items-center justify-center h-[12rem] ${item.color} border-white/50 border-[1px] rounded-xl `}>
+                                <h2 className='font-semibold text-[1.2rem]'>{item.event}</h2>
+                                <h2 className={`font-bold text-[2rem] ${item.text}`}>{item.prize}</h2>
+                                </div>
+                        ))
+                    }
+                </div>
+
             </div>
-        </div>
 
-
+            {/* <Heading>Competetions</Heading>
+            --to be added later-- */}
         </div>
         <FootComp/>
     </main>
