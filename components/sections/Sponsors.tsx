9274f098c6a1dfac5720df21ad88b3c7092d55be
Ritesh Nandy <ritesh.nandy2002@gmail.com>
2024-07-21 01:00:40 +0530

    small changes

--- components/sections/Sponsors.tsx
+++ components/sections/Sponsors.tsx
@@ -61,10 +61,10 @@ export const Sponsors = () => {
 
         <div className='md:px-20 px-4 md:pt-16 pt-20 rounded-t-xl border-t-[2px] min-h-screen border-yellow-700 w-full  my-10 sticky top-0 bg-gradient-to-b from-yellow-900 via-yellow-700 to-yellow-800'>
             <h1 className='bg-gradient-to-b from-yellow-600 via-yellow-200 to-yellow-400 text-transparent bg-clip-text text-center w-full mx-auto md:text-[3.3rem] text-[2.3rem] font-bold' >Media</h1>
-            <div className='my-20 w-[90%] mx-auto grid grid-cols-2 items-center justify-center max-md:grid-cols-2 gap-20'>
+            <div className='my-20 w-[90%] mx-72 grid grid-cols-3 items-center justify-center max-md:grid-cols-2 gap-20'>
             {medialist.map((item, index)=>(
                 <div className=' overflow-hidden w-full h-full'>
-                <Image src={item} alt="partner1" className='w-full h-full object-contain bg-black'/>
+                <Image src={item} alt="partner1" className='w-full h-full object-contain bg-transparent'/>
                 </div>))}
             </div>
         </div>
@@ -73,10 +73,10 @@ export const Sponsors = () => {
             <h1 className='bg-gradient-to-b from-gray-600 via-gray-200 to-gray-400 text-transparent bg-clip-text text-center w-full mx-auto md:text-[3.3rem] text-[2.3rem] font-bold' >Exclusive Social Media Platform </h1>
             <h1 className='bg-gradient-to-b from-yellow-600 via-yellow-200 to-yellow-400 text-transparent bg-clip-text text-center w-full mx-auto md:text-[3.3rem] text-[2.3rem] font-bold' >⭐️⭐️⭐️⭐️⭐️</h1>
             <div>
-            <div className='my-20 w-[90%] mx-auto grid grid-cols-2 items-center justify-center max-md:grid-cols-2 gap-20'>
+            <div className='my-20 w-[40%] mx-auto grid grid-cols-1 items-center justify-center max-md:grid-cols-2 gap-20'>
             {socialPart.map((item, index)=>(
                 <div className=' overflow-hidden w-full h-full'>
-                <Image src={item} alt="partner1" className='w-full h-full object-contain bg-black'/>
+                <Image src={item} alt="partner1" className='w-full h-full object-contain bg-transparent'/>
                 </div>))}
             </div> 
                 
