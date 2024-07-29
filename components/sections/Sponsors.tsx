a9a6e77e8eed7bccf70bbed7fc502583e6f8d5ed
Saptarshi Pal <96665747+Saptarshi1810@users.noreply.github.com>
2024-07-29 12:02:36 +0530

    added and changed

--- components/sections/Sponsors.tsx
+++ components/sections/Sponsors.tsx
@@ -15,10 +15,14 @@ import kfc from '@/assets/sponsors/FOOD/KFC.jpeg'
 import shaw from '@/assets/sponsors/FOOD/SHAW.jpeg'
 
 import friends from '@/assets/sponsors/Media/friendsFM.jpeg'
-import mim from '@/assets/sponsors/Media/mim.jpeg'
+import mim from '@/assets/sponsors/Media/mim.png'
 import kol from '@/assets/sponsors/Media/kolkata.png'
+import B from '@/assets/sponsors/Media/B-Prod.jpg'
 
-const medialist =[friends,mim]
+import momo from '@/assets/sponsors/FOOD/WOW-momo.jpg'
+import china from '@/assets/sponsors/FOOD/wow China.jpg'
+
+const medialist =[friends,mim,B]
 
 const socialPart=[
 kol
@@ -29,7 +33,7 @@ const sponsorList = [
   ];
 
 const foodList = [
-    campa,kfc
+    campa,kfc,momo,china
   ];
   
 
@@ -51,7 +55,7 @@ export const Sponsors = () => {
 
         <div className='md:px-20 px-4 md:pt-16 pt-20 rounded-t-xl border-t-[2px] min-h-screen border-gray-700 w-full  my-10 sticky top-0 bg-gradient-to-b from-gray-900 via-gray-700 to-gray-800'>
             <h1 className='bg-gradient-to-b from-gray-700 via-gray-300 to-gray-500 text-transparent bg-clip-text text-center w-full mx-auto md:text-[3.3rem] text-[2.3rem] font-bold' >Food & Beverages</h1>
-            <div className='my-20 w-[70%]  mx-auto grid grid-cols-2 items-center justify-center max-md:grid-cols-2 gap-20 align-middle'>
+            <div className='my-20 w-[70%]  mx-auto grid grid-cols-4 items-center justify-center max-md:grid-cols-2 gap-20 align-middle'>
             {foodList.map((item, index)=>(
                 <div className=' overflow-hidden w-full h-full'>
                 <Image src={item} alt="partner1" className='w-[20rem] h-full rounded-xl border-[2px] object-contain  bg-black'/>
@@ -61,7 +65,7 @@ export const Sponsors = () => {
 
         <div className='md:px-20 px-4 md:pt-16 pt-20 rounded-t-xl border-t-[2px] min-h-screen border-yellow-700 w-full  my-10 sticky top-0 bg-gradient-to-b from-yellow-900 via-yellow-700 to-yellow-800'>
             <h1 className='bg-gradient-to-b from-yellow-600 via-yellow-200 to-yellow-400 text-transparent bg-clip-text text-center w-full mx-auto md:text-[3.3rem] text-[2.3rem] font-bold' >Media</h1>
-            <div className='my-20 w-[80%] mx-auto grid grid-cols-2 items-center justify-center max-md:grid-cols-2 gap-20'>
+            <div className='my-20 w-[80%] mx-auto grid grid-cols-3 items-center justify-center max-md:grid-cols-2 gap-20'>
             {medialist.map((item, index)=>(
                 <div className=' overflow-hidden w-full h-full'>
                 <Image src={item} alt="partner1" className='w-full h-full rounded-xl border-[2px] object-contain bg-black'/>
